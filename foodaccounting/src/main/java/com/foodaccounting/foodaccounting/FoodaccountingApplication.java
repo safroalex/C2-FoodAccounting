a85d7732ecef1aea11192881f35ea67884215600
safroalex <alexsafr.a@ya.ru>
2024-05-11 14:36:20 +0300

    init proj

---
+++ foodaccounting/src/main/java/com/foodaccounting/foodaccounting/FoodaccountingApplication.java
@@ -0,0 +1,13 @@
+package com.foodaccounting.foodaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodaccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodaccountingApplication.class, args);
+	}
+
+}
