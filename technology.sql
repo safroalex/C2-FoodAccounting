c1084f68e138641ba90b30132b3ac01031b8b0de
safroalex <alexsafr.a@ya.ru>
2024-05-13 11:19:05 +0300

    add techbd

--- technology.sql
+++ technology.sql
@@ -1,84 +1,31 @@
--- Таблица "Раскладка"
+-- Таблица раскладок
 CREATE TABLE Layout (
-    ID UUID PRIMARY KEY,
-    TechnologicalCardId UUID REFERENCES TechnologicalCard(ID),
-    Number INTEGER,
+    ID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     DateBegin DATE,
-    Date DATE,
     DateEnd DATE,
-    CategoryPersonsId UUID REFERENCES CategoryPersons(ID),
-    WeekId UUID REFERENCES Week(ID),
-    TypeFoodId UUID REFERENCES TypeFood(ID),
-    TypesDishId UUID REFERENCES TypesDish(ID),
-    Calorific1 INTEGER,
-    Calorific2 INTEGER,
-    Calorific3 INTEGER,
-    Calorific4 INTEGER,
-    Calorific5 INTEGER
+    Status VARCHAR(50)
 );
 
--- Справочник "Ингредиенты"
-CREATE TABLE Ingredients (
-    ID UUID PRIMARY KEY,
-    TechnologicalCardId UUID REFERENCES TechnologicalCard(ID),
-    ProduktId UUID REFERENCES Produkt(ID),
-    WeightGross FLOAT,
-    WeightNet FLOAT
+-- Таблица блюд
+CREATE TABLE Dish (
+    ID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Name VARCHAR(100),
+    CaloricContent INT
 );
 
--- Таблица "Количество продовольствия"
-CREATE TABLE QuantityProdukt (
-    ID UUID PRIMARY KEY,
-    WarehouseId UUID REFERENCES Warehouse(ID),
-    ProduktGroupId UUID REFERENCES ProduktGroup(ID),
-    ProduktId UUID REFERENCES Produkt(ID),
-    Number VARCHAR(5),
-    UnitId UUID REFERENCES Unit(ID),
-    DateExpiry DATE,
-    Remark TEXT
+-- Таблица ингредиентов блюда
+CREATE TABLE DishIngredients (
+    ID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    DishId UUID REFERENCES Dish(ID),
+    ProduktId UUID REFERENCES Product(ID),
+    Quantity DECIMAL,
+    UnitId UUID REFERENCES Unit(ID)
 );
 
--- Таблица "Справка о спецконтингенте"
-CREATE TABLE Convict (
-    Id UUID PRIMARY KEY,
-    QuantityConvictId UUID REFERENCES QuantityConvict(ID),
-    AdditionDocId UUID REFERENCES AdditionDoc(ID),
-    Number INTEGER,
-    Date DATE,
-    All INTEGER,
-    IsAddition BOOLEAN
+-- Таблица блюд в раскладке
+CREATE TABLE LayoutDishes (
+    ID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    LayoutId UUID REFERENCES Layout(ID),
+    DishId UUID REFERENCES Dish(ID),
+    Quantity INT
 );
-
--- Справочник "Технологическая карта"
-CREATE TABLE TechnologicalCard (
-    ID UUID PRIMARY KEY,
-    IngredientsId UUID REFERENCES Ingredients(ID),
-    Name TEXT,
-    ShortName TEXT,
-    TypesDishId UUID REFERENCES TypesDish(ID),
-    WeightOfDishes FLOAT,
-    Calorific INTEGER,
-    Protein FLOAT,
-    Fat FLOAT,
-    Carbohydrates FLOAT,
-    Preparation TEXT,
-    OrganolepticCharacteristic TEXT,
-    Requirement TEXT
-);
--- Справочник "Продукты" (Produkt)
-CREATE TABLE Produkt (
-    ID UUID PRIMARY KEY,
-    ProduktGroupID UUID REFERENCES ProduktGroup(ID),
-    Name VARCHAR,
-    Mark BOOLEAN,
-    Replacement BOOLEAN,
-    Stomach BOOLEAN
-);
-
--- Справочник "Категории спецконтингента" (CategoryPersons)
-CREATE TABLE CategoryPersons (
-    ID UUID PRIMARY KEY,
-    Name VARCHAR,
-    Mark INTEGER
-);
-
