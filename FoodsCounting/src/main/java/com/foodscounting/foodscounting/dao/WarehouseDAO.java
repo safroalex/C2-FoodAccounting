bafbedc95cb627dc1ad1f152d4cc2ba15633f441
safroalex <alexsafr.a@ya.ru>
2024-05-13 11:51:22 +0300

    add dishes

--- FoodsCounting/src/main/java/com/foodscounting/foodscounting/dao/WarehouseDAO.java
+++ FoodsCounting/src/main/java/com/foodscounting/foodscounting/dao/WarehouseDAO.java
@@ -166,7 +166,4 @@ public class WarehouseDAO {
         return products;
     }
 
-
-
-
 }
--- technology.sql
+++ technology.sql
@@ -29,3 +29,78 @@ CREATE TABLE LayoutDishes (
     DishId UUID REFERENCES Dish(ID),
     Quantity INT
 );
+
+
+
+
+
+
+
+-- Добавление блюд
+INSERT INTO Dish (Name, CaloricContent) VALUES
+('Каши вязкие', 215),
+('Каша молочная', 200),
+('Кисель из концентрата', 88),
+('Макаронные изделия отварные', 200),
+('Мясо отварное для вторых блюд', 290),
+('Пюре картофельное', 510),
+('Сосиски отварные', 260),
+('Суп картофельный на мясокостном бульоне с крупой рисовой', 260),
+('Рагу из овощей', 150);
+
+-- Каши вязкие
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Каши вязкие'), (SELECT ID FROM Product WHERE Name = 'Крупа'), 55, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Каши вязкие'), (SELECT ID FROM Product WHERE Name = 'Морковь'), 10, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Каши вязкие'), (SELECT ID FROM Product WHERE Name = 'Лук репчатый'), 10, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Каши вязкие'), (SELECT ID FROM Product WHERE Name = 'Кулинарные жиры'), 5, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+-- Каша молочная
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Каша молочная'), (SELECT ID FROM Product WHERE Name = 'Молоко'), 100, (SELECT ID FROM Unit WHERE Name = 'миллилитры')),
+((SELECT ID FROM Dish WHERE Name = 'Каша молочная'), (SELECT ID FROM Product WHERE Name = 'Крупы'), 40, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+-- Кисель из концентрата
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Кисель из концентрата'), (SELECT ID FROM Product WHERE Name = 'Концентрат киселя'), 25, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+
+-- Макаронные изделия отварные
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Макаронные изделия отварные'), (SELECT ID FROM Product WHERE Name = 'Макаронные изделии'), 49, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+-- Мясо отварное для вторых блюд
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Мясо отварное для вторых блюд'), (SELECT ID FROM Product WHERE Name = 'Мясо сырое'), 90, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Мясо отварное для вторых блюд'), (SELECT ID FROM Product WHERE Name = 'Соль'), 1, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+-- Сосиски отварные
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Сосиски отварные'), (SELECT ID FROM Product WHERE Name = 'Сосиски'), 80, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+-- Суп картофельный на мясокостном бульоне с крупой рисовой
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Суп картофельный на мясокостном бульоне с крупой рисовой'), (SELECT ID FROM Product WHERE Name = 'Мука II сорт'), 2, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Суп картофельный на мясокостном бульоне с крупой рисовой'), (SELECT ID FROM Product WHERE Name = 'Морковь'), 10, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Суп картофельный на мясокостном бульоне с крупой рисовой'), (SELECT ID FROM Product WHERE Name = 'Лук репчатый'), 10, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Суп картофельный на мясокостном бульоне с крупой рисовой'), (SELECT ID FROM Product WHERE Name = 'Томатная паста'), 1, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Суп картофельный на мясокостном бульоне с крупой рисовой'), (SELECT ID FROM Product WHERE Name = 'Кулинарные жиры'), 10, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Суп картофельный на мясокостном бульоне с крупой рисовой'), (SELECT ID FROM Product WHERE Name = 'Крупа рисовая'), 15, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+-- Рагу из овощей
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Рагу из овощей'), (SELECT ID FROM Product WHERE Name = 'Картофель'), 50, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Рагу из овощей'), (SELECT ID FROM Product WHERE Name = 'Морковь'), 30, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Рагу из овощей'), (SELECT ID FROM Product WHERE Name = 'Свекла'), 30, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Рагу из овощей'), (SELECT ID FROM Product WHERE Name = 'Лук репчатый'), 15, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Рагу из овощей'), (SELECT ID FROM Product WHERE Name = 'Масло растительное'), 20, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Рагу из овощей'), (SELECT ID FROM Product WHERE Name = 'Мука'), 3, (SELECT ID FROM Unit WHERE Name = 'грамм'));
+
+-- Пюре картофельное
+INSERT INTO DishIngredients (DishId, ProduktId, Quantity, UnitId) VALUES
+((SELECT ID FROM Dish WHERE Name = 'Пюре картофельное'), (SELECT ID FROM Product WHERE Name = 'Кулинарные жиры'), 5, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Пюре картофельное'), (SELECT ID FROM Product WHERE Name = 'Масло растительное'), 5, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Пюре картофельное'), (SELECT ID FROM Product WHERE Name = 'Морковь'), 15, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Пюре картофельное'), (SELECT ID FROM Product WHERE Name = 'Лук репчатый'), 20, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Пюре картофельное'), (SELECT ID FROM Product WHERE Name = 'Томатная паста'), 1, (SELECT ID FROM Unit WHERE Name = 'грамм')),
+((SELECT ID FROM Dish WHERE Name = 'Пюре картофельное'), (SELECT ID FROM Product WHERE Name = 'Мясо отварное'), 90, (SELECT ID FROM Unit WHERE Name = 'грамм'));
